3aad39ca5fb1d6586d208799676819cb63e63e7f
Florian Kretlow <floriankretlow@gmx.de>
2020-08-21 14:47:10 +0200

    doesn't work like this, revert to only the export here

--- src/routes/kontakt/api.js
+++
@@ -1,8 +0,0 @@
-/* Don't push this file to GitHub. Sapper imports the url of our message handler on export,
- * but we want to keep it as secret as possible. (Whoever who wants to know it can easily
- * read it out in the browser, but still...)
- */
-
-module.exports = {
-    MESSAGE_API: 'https://louise-mail.vercel.app/api/mail',
-};
--- src/routes/termine.svelte
+++
@@ -1,5 +0,0 @@
-<h1>Termine</h1>
-<ul>
-    <li>Nächster Termin...</li>
-    <li>Nächster Termin...</li>
-</ul>
